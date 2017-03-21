2b4f445d51168065584c89231de400597939ae5c
Matt Massie <massie@cs.berkeley.edu>
2017-03-21 09:07:27 -0700

    First version of alternativefax

---
+++ README.md
@@ -0,0 +1,36 @@
+# alternativefax
+
+* Conway: "You're saying it's a falsehood and Sean Spicer, our press secretary, gave alternative facts to that."
+* Todd: "Alternative facts are not facts. They are falsehoods."
+
+My ISP has a free fax service form that requires only a phone number and PDF file. This simple program allows me to autogenerate a PDFs to my representatives, senators, committee members, etc. I'm sharing it here in the hopes that it might be useful to others. Pull requests are welcomed.
+
+To install alternativefax:
+
+1. Download [the latest version of alternativefax](https://github.com/massie/alternativefax/archive/master.zip) (or use `git clone`)
+2. Make sure you have the `latex` Python module installed, e.g. `pip install latex`
+3. Create a signature image and save it to e.g. `signature.png`.
+4. Copy the `sender.csv.example` file to `sender.csv` and update the fields. Make sure that you the path to your signature file matches with step 3 above.
+5. Update the `addressees.csv` file to contain the people that you want to generate faxes for.
+
+*WARNING:* be careful not to check in your `signature.png` or `sender.csv` file into git since it contains personal information. You can use e.g. `git grep <name>` to see if your information leaked into git.
+
+To use alternativefax:
+
+```bash
+$ cat > message.txt
+This is a test message. \textit{DO NOT FAX THIS}
+^D
+$ ./generate.py message.txt
+$ ls *.pdf
+test.txt.committee_on_oversight_and_government_reform.pdf
+test.txt.committee_on_ways_and_means.pdf
+test.txt.congressman_mark_desaulnier.pdf
+test.txt.governor_jerry_brown.pdf
+test.txt.senator_dianne_feinstein.pdf
+test.txt.senator_kamala_harris.pdf
+```
+
+Here's an example of one of the generated faxes.
+
+![Example FAX](example_fax.png)
---
+++ generate.py
@@ -0,0 +1,50 @@
+#!/usr/bin/ENV python
+# coding: utf-8
+"""
+Generate faxes to send to list of addressees
+"""
+import csv
+import os
+import sys
+from jinja2 import FileSystemLoader
+from latex.jinja2 import make_env
+from latex import build_pdf
+
+if len(sys.argv) != 2:
+    print "Provide the path to the file containing the text of the body of the faxes."
+    print "e.g.> %s file.txt" % (sys.argv[0])
+    sys.exit(1)
+
+FAXPATH = sys.argv[1]
+with open(FAXPATH, 'r') as myfile:
+    FAXTEXT = myfile.read().replace('\n', '')
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+ENV = make_env(loader=FileSystemLoader("."))
+TEMPLATE = ENV.get_template('fax.latex')
+
+with open("sender.csv") as sendercsv:
+    SENDER = csv.DictReader(sendercsv).next()
+
+ADDRESSEES = []
+with open("addressees.csv") as addressees:
+    for entry in csv.DictReader(addressees):
+        # Addresses are '|' delimited. Convert to latex newlines '\\'
+        entry['addressee'] = "\\\\".join(entry['address'].split("|")) +\
+                             "\\\\FAX: %s" % entry['fax_number']
+        # Create a filename for this addressee
+        entry['filename'] = entry['greeting'].lower().replace(' ', '_') + ".pdf"
+        entry['signature'] = SENDER['signature_file']
+        entry['faxtext'] = FAXTEXT
+        entry['sendername'] = SENDER['name']
+        entry['senderzip'] = SENDER['zip']
+        entry['sendercity'] = SENDER['city']
+        entry['senderaddress'] = "\\\\".join([SENDER['address'], "%s, %s %s" %\
+		                      (SENDER['city'], SENDER['state'], SENDER['zip']),\
+							  "Email: " + SENDER['email'], "Phone: " + SENDER['phone']])
+        entry['constituent'] = True if entry['constituent'] == 'True' else False
+        ADDRESSEES.append(entry)
+
+for addressee in ADDRESSEES:
+    pdf = build_pdf(TEMPLATE.render(addressee), texinputs=[CURRENT_DIR, ''])
+    pdf.save_to(FAXPATH + "." + addressee['filename'])
